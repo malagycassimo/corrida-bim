136e5dcdd82afca29e728b4e37a43f67ba02df1a
Malagy Cassimo <malagyy@gmail.com>
2026-09-10 00:56:46 +0200

    Refactor code structure for improved readability and maintainability

--- client/src/utils/assets.ts
+++ client/src/utils/assets.ts
@@ -8,46 +8,57 @@ export const carouselImages: CarouselImageProps[] = [
     {
         alt: "16ª Corrida Millennium bim",
         src: "/assets/images/carousel/carousel-item-1.webp",
+        src: "/assets/images/carrosel-new/DSC_4099.jpg",
     },
     {
         alt: "16ª Corrida Millennium bim",
         src: "/assets/images/carousel/carousel-item-2.webp",
+        src: "/assets/images/carrosel-new/DSC_4217.jpg",
     },
     {
         alt: "16ª Corrida Millennium bim",
         src: "/assets/images/carousel/carousel-item-3.webp",
+        src: "/assets/images/carrosel-new/DSC_4271.jpg",
     },
     {
         alt: "16ª Corrida Millennium bim",
         src: "/assets/images/carousel/carousel-item-4.webp",
+        src: "/assets/images/carrosel-new/DSC_4289.jpg",
     },
     {
         alt: "16ª Corrida Millennium bim",
         src: "/assets/images/carousel/carousel-item-5.webp",
+        src: "/assets/images/carrosel-new/DSC_4394.jpg",
     },
     {
         alt: "16ª Corrida Millennium bim",
         src: "/assets/images/carousel/carousel-item-6.webp",
+        src: "/assets/images/carrosel-new/DSC_4759.jpg",
     },
     {
         alt: "16ª Corrida Millennium bim",
         src: "/assets/images/carousel/carousel-item-7.webp",
+        src: "/assets/images/carrosel-new/DSC_4776.jpg",
     },
     {
         alt: "16ª Corrida Millennium bim",
         src: "/assets/images/carousel/carousel-item-8.webp",
+        src: "/assets/images/carrosel-new/DSC_4910.jpg",
     },
     {
         alt: "16ª Corrida Millennium bim",
         src: "/assets/images/carousel/carousel-item-9.webp",
+        src: "/assets/images/carrosel-new/DSC_5374.jpg",
     },
     {
         alt: "16ª Corrida Millennium bim",
         src: "/assets/images/carousel/carousel-item-10.webp",
+        src: "/assets/images/carrosel-new/DSC_6590.jpg",
     },
     {
         alt: "16ª Corrida Millennium bim",
         src: "/assets/images/carousel/carousel-item-11.webp",
+        src: "/assets/images/carrosel-new/PHOTO-2026-09-09-16-10-28.jpg",
     },
     {
         alt: "16ª Corrida Millennium bim",
